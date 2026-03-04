88a0213bf1532107ec54a018ec1e6440a3768be6
Goner <skrapziboom@gmail.com>
2026-03-04 17:28:10 +0300

    query, modal

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,6 +1,5 @@
 export { useInterval } from "./useInterval";
 export { useOnlineStatus } from "./useOnlineStatus";
 export { useCoinList } from "./useCoinList";
-export { useCryptoPrice } from "./useCryptoPrice";
 export { useCryptoPrices } from "./useCryptoPrices";
 export { useHistory24h } from "./useHistory24h";
